82e6d246ac58897f7b7b6cc3b95a49b795bd5973
mwang117 <michel.wang@etu.unilim.fr>
2025-03-05 14:51:10 +0100

    ajout label simple dans la troisième page

--- MichelAPP/obj/Debug/net9.0-ios/iossimulator-x64/MichelAPP.AssemblyInfo.cs
+++ MichelAPP/obj/Debug/net9.0-ios/iossimulator-x64/MichelAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e32db0c74e180c199b86560e8fdac3a87fd9f30c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+51d5805d58dab605242ce2e97ba1ac114f6386e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
