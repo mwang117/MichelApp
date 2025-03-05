55255739e36a36c34bb123ddc65bff78845c0ed5
mwang117 <michel.wang@etu.unilim.fr>
2025-03-05 14:31:56 +0100

    création fichier pour chaque onglet : .xaml et .xaml.cs

--- MichelAPP/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MichelAPP.AssemblyInfo.cs
+++ MichelAPP/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MichelAPP.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6491bd1934130402520f8083487a2d080ab4a77b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+029aa45e80ccf74cbcece6a9e8393f3378906463")]
 [assembly: System.Reflection.AssemblyProductAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
