828a62c357135397ff96a8e4e71471083fc1e3d0
mwang117 <michel.wang@etu.unilim.fr>
2025-03-06 13:20:34 +0100

    changmenet visuelle pour la troisième page + ajout bouton pour pouvoir selectionner une image à ajouter

--- MichelAPP/ViewModels/CoffeeViewModel .cs
+++ MichelAPP/ViewModels/CoffeeViewModel .cs
@@ -4,6 +4,8 @@ using MichelAPP.Models;
 using MichelAPP.Services;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
 
 namespace MichelAPP.ViewModels
 {
@@ -23,10 +25,22 @@ namespace MichelAPP.ViewModels
             }
         }
 
+        // Propriétés pour le formulaire
+        public string NewTitle { get; set; } = "";
+        public string NewDescription { get; set; } = "";
+        public string NewImage { get; set; } = "";
+        public string NewIngredients { get; set; } = "";
+
+        // Commande pour ajouter un café
+        public ICommand AddCoffeeCommand { get; }
+
         public CoffeeViewModel()
         {
             _coffeeService = new CoffeeService();
             LoadCoffees();
+
+            // Définition de la commande Ajouter
+            AddCoffeeCommand = new Command(AddCoffee);
         }
 
         private async void LoadCoffees()
@@ -39,6 +53,34 @@ namespace MichelAPP.ViewModels
                 Coffees.Add(coffee);
             }
             IsLoading = false;
+        }
+
+        // Méthode pour ajouter un café à la liste
+        private void AddCoffee()
+        {
+            if (string.IsNullOrWhiteSpace(NewTitle) || string.IsNullOrWhiteSpace(NewDescription))
+                return;
+
+            var newCoffee = new CoffeeModel
+            {
+                Id = Coffees.Count + 1,
+                Title = NewTitle,
+                Description = NewDescription,
+                Image = string.IsNullOrWhiteSpace(NewImage) ? "default.png" : NewImage,
+                Ingredients = NewIngredients.Split(',')
+            };
+
+            Coffees.Add(newCoffee);
+
+            // Réinitialisation des champs
+            NewTitle = "";
+            NewDescription = "";
+            NewImage = "";
+            NewIngredients = "";
+            OnPropertyChanged(nameof(NewTitle));
+            OnPropertyChanged(nameof(NewDescription));
+            OnPropertyChanged(nameof(NewImage));
+            OnPropertyChanged(nameof(NewIngredients));
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
--- MichelAPP/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MichelAPP.AssemblyInfo.cs
+++ MichelAPP/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MichelAPP.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4ddfae224b12e74d002c2cec6a201905cc3ea7c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2ea5665451e192766cebf6f96933537f65ece667")]
 [assembly: System.Reflection.AssemblyProductAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
