30791a18894032b3297de7ecc609c23acce9fb94
mwang117 <michel.wang@etu.unilim.fr>
2025-03-06 16:47:06 +0100

    ajout du titre pour le visuel de la page

--- MichelAPP/obj/Debug/net9.0-android/MichelAPP.AssemblyInfo.cs
+++ MichelAPP/obj/Debug/net9.0-android/MichelAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ea639d9b79e40fba2c3ca1b71b184e5ac8ec5b8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+958605ebb45404a7500024a71da2708786a586e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
