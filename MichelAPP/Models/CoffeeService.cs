83d78f5aed4ca2a02af264bca4f9a019bffb8958
mwang117 <michel.wang@etu.unilim.fr>
2025-03-06 11:28:18 +0100

    création fichier du service CoffeeService.cs

---
+++ MichelAPP/Models/CoffeeService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MichelAPP.Models;
+
+namespace MichelAPP.Services
+{
+    public class CoffeeService
+    {
+        private const string ApiUrl = "https://api.sampleapis.com/coffee/hot";
+        private readonly HttpClient _httpClient;
+
+        public CoffeeService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<CoffeeModel>> GetCoffeesAsync()
+        {
+            var response = await _httpClient.GetStringAsync(ApiUrl);
+            return JsonSerializer.Deserialize<List<CoffeeModel>>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
