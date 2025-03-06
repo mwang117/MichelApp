0d4aab9a62f3895f11733237199dca3f1c14f7bf
mwang117 <michel.wang@etu.unilim.fr>
2025-03-06 11:34:11 +0100

    modif deuximee page xaml pour afficher le café

--- MichelAPP/obj/Debug/net9.0-android/MichelAPP.AssemblyInfo.cs
+++ MichelAPP/obj/Debug/net9.0-android/MichelAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dab3a1b21dc68e789c297d52f719b27dc9c329ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c6a3d5ea4c37c8441c62f1c3f02d33b01b97424f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MichelAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
